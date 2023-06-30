391a1f424a008db8d73a48d835ce3afda355816a
José <josevillagran18@gmail.com>
2023-06-30 18:10:46 -0400

    feat: reformulate package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="playing_with_commitzen_and_semantic_release",
+    version="0.0.1",
+    description="Playing with commitzen and semantic release",
+    author="José Villagrán",
+    author_email= "josevillagran18@gmail.com"
+)
