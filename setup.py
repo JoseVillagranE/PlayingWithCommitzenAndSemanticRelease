a8c0475cfbe19b00d6720e3779d2cffd45bad032
José  Villagran Escobar <jose.villagran@femsasalud.com>
2023-07-03 12:49:27 -0400

    bump: version 0.10.0-dev1 → 0.10.0-rc0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="playing_with_commitzen_and_semantic_release",
-    version="0.10.0-dev1",
+    version="0.10.0-rc0",
     description="Playing with commitzen and semantic release",
     author="José Villagrán",
     author_email= "josevillagran18@gmail.com"
